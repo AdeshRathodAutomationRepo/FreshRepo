93439f77b56fbccb1f478b131535ac315481e94c
Adesh <adeshrathod989@gmail.com>
2024-05-22 03:23:47 +0530

    done small changes

--- fresh/src/test/java/noobie/Lesson2.java
+++ fresh/src/test/java/noobie/Lesson2.java
@@ -1,11 +1,15 @@
 package noobie;
 
+import org.testng.annotations.Test;
+
 public class Lesson2 {
 
+	@Test
 	public void addTwoNumber() {
 		int a=10;
 		int b=20;
 		
 		int c=a+b;
+		System.out.println(c);
 	}
 }
